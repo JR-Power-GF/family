0a9ceeb6950f221b61257a2ca705b223366b68ed
Gaofang <845469177@qq.com>
2026-03-29 13:57:36 +0800

    feat(app): add network listener for auto-sync, recover stale items

--- App.vue
+++ App.vue
@@ -1,29 +1,46 @@
-<script>
-import { cacheManager } from './utils/cache.js'
+<script setup>
+import { cacheManager, CacheKeys } from './utils/cache.js'
+import { recoverStaleItems } from './utils/offline.js'
+import { syncManager } from './utils/syncManager.js'
 
-export default {
-  onLaunch: function() {
-    console.log('App Launch')
+onLaunch(() => {
+  console.log('App Launch')
 
-    // Initialize WeChat Cloud
-    if (wx.cloud) {
-      wx.cloud.init({
-        env: 'cloud1-6geq4sla3d88052d',
-        traceUser: true
-      })
-      console.log('WeChat Cloud initialized')
-    }
-
-    // Clear expired cache entries on startup
-    cacheManager.clearExpired()
-  },
-  onShow: function() {
-    console.log('App Show')
-  },
-  onHide: function() {
-    console.log('App Hide')
+  // Initialize WeChat Cloud
+  if (wx.cloud) {
+    wx.cloud.init({
+      env: 'cloud1-6geq4sla3d88052d',
+      traceUser: true
+    })
+    console.log('WeChat Cloud initialized')
   }
-}
+
+  // Clear expired cache entries
+  cacheManager.clearExpired()
+
+  // Recover stale items (app killed during sync)
+  recoverStaleItems()
+
+  // Listen for network changes
+  uni.onNetworkStatusChange((res) => {
+    console.log(`[Network] ${res.isConnected ? 'Connected' : 'Disconnected'}`)
+
+    if (res.isConnected) {
+      syncManager.syncAll()
+    }
+  })
+
+  // Trigger initial sync if online
+  syncManager.syncAll()
+})
+
+onShow(() => {
+  console.log('App Show')
+})
+
+onHide(() => {
+  console.log('App Hide')
+})
 </script>
 
 <style lang="scss">
